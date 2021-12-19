0f762c1aeda624961d7c0d368ace7c500d172ba2
Ifegwu <agbanyimdaniel29@gmail.com>
2021-12-19 18:29:36 +0100

    implemented drag the columns

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,9 +1,12 @@
-// import { FC } from 'react';
+import { useRef } from "react"
+import { useDrop } from "react-dnd";
 import { CardContainer, ColumnTitle } from './styles';
 import { AddNewItem } from "./AddNewItem"
 import { useAppState } from "./state/AppStateContext"
 import { Card } from "./Card"
-import { addTask } from "./state/actions"
+import { addTask, moveList } from "./state/actions"
+import { useItemDrag } from "./utils/useItemDrag"
+import { isHidden } from "./utils/isHidden"
 
 type ColumnProps = {
     text: string
@@ -12,10 +15,34 @@ type ColumnProps = {
 
 export const Column = ({ text, id }: ColumnProps) => {
 
-    const { getTasksByListId, dispatch } = useAppState()
+    const { draggedItem, getTasksByListId, dispatch } = useAppState()
     const tasks = getTasksByListId(id)
+    const ref = useRef<HTMLDivElement>(null)
+
+    const [, drop] = useDrop({
+        accept: "COLUMN",
+        hover() {
+            if (!draggedItem) {
+                return
+            }
+            if (draggedItem.type === "COLUMN") {
+                if (draggedItem.id === id) {
+                    return
+                }
+                dispatch(moveList(draggedItem.id, id))
+            }
+        }
+    })
+
+    const { drag } = useItemDrag({ type: "COLUMN", id, text })
+
+    drag(drop(ref))
+
     return (
-        <CardContainer>
+        <CardContainer 
+            ref={ref}
+            isHidden={isHidden(draggedItem, "COLUMN", id)}
+        >
             <ColumnTitle>{text}</ColumnTitle>
             {tasks.map(task => <Card text={task.text} key={task.id} id={task.id} />)}
             <AddNewItem 
--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,3 +1,5 @@
+import { DragItem } from "../DragItem"
+
 export type Action = 
     |   {
             type: "ADD_LIST", payload: string
@@ -5,6 +7,14 @@ export type Action =
     |   { 
             type: "ADD_TASK", payload: {text: string, listId: string}
         } 
+    |   {
+            type: "MOVE_LIST", payload: {draggedId: string, hoverId: string}
+
+        }
+    |   {
+            type: "SET_DRAGGED_ITEM",
+            payload: DragItem | null
+        }
 
 export const addTask = (
     text: string,
@@ -22,4 +32,22 @@ export const addList = (
 ): Action =>({
     type: "ADD_LIST",
     payload: text
+})
+
+export const moveList = (
+    draggedId: string,
+    hoverId: string,
+): Action => ({
+    type: "MOVE_LIST",
+    payload: {
+        draggedId,
+        hoverId,
+    }
+})
+
+export const setDraggedItem = (
+    draggedItem: DragItem | null,
+): Action => ({
+    type: "SET_DRAGGED_ITEM",
+    payload: draggedItem
 })
